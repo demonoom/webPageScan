d326e08ecc8fe4545a5f0d4f6d9510c49d8130fd
noom <noom0130@foxmail.com>
2018-07-06 16:22:39 +0800

    .

--- static/arBook/asset/js/index.js
+++ static/arBook/asset/js/index.js
@@ -1,5 +1,5 @@
 $(function () {
-    if (window.location.href != 'https://localhost:6443/arBook/') {
+    if (window.location.href != 'https://www.maaee.com:6443/arBook/') {
         document.querySelector('#sharePage').style.display = 'block';
     }
 });
