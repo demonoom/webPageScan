98e7452fa42b43c50f705deb2f404a443161e277
noom <noom0130@foxmail.com>
2018-07-09 09:49:53 +0800

    .

--- static/arBook/asset/js/index.js
+++ static/arBook/asset/js/index.js
@@ -1,11 +1,18 @@
 $(function () {
-        if (window.location.href != 'https://www.maaee.com:6443/arBook/') {
-            // if (window.location.href != 'https://172.16.2.128:6443/arBook/') {
+        // if (window.location.href != 'https://www.maaee.com:6443/arBook/') {
+        if (window.location.href != 'https://localhost:6443/arBook/') {
             document.querySelector('#sharePage').style.display = 'block';
             document.querySelector('#start').style.position = 'absolute'
             document.querySelector('#stop').style.position = 'absolute'
         } else {
             document.querySelector('#openCamera').click()
+        }
+        var phoneType = navigator.userAgent;
+
+        if (phoneType.indexOf('iPhone') > -1 || phoneType.indexOf('iPad') > -1) {
+            if (phoneType.indexOf('MicroMessenger') > -1) {
+
+            }
         }
     }
 );
